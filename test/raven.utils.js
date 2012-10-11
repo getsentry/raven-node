9edf32a3b76f4ce2af289baed9d0455e65e30104
Roman Shtylman <shtylman@gmail.com>
2012-10-10 23:56:06 -0400

    error parsing using stackback

--- test/raven.utils.js
+++ test/raven.utils.js
@@ -1,8 +1,9 @@
-var raven = require('../')
-  , fs = require('fs')
-  , glob = require('glob')
-  , path = require('path')
-  , should = require('should');
+var raven = require('../');
+var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
+var should = require('should');
+var vm = require('vm');
 
 describe('raven.utils', function() {
   describe('#constructChecksum()', function(){
@@ -33,98 +34,74 @@ describe('raven.utils', function() {
   });
 
   describe('#parseStack()', function(){
-    var stack = fs.readFileSync(__dirname + '/fixtures/stack.txt', 'utf8');
+    var src = fs.readFileSync(__dirname + '/fixtures/stack.js', 'utf8');
+
+    // capture error from running the invalid file
+    var error;
+    try {
+      vm.runInThisContext(src, './test/fixtures/stack.js');
+    } catch (e) {
+      error = e;
+    }
 
     it('should not throw an error', function(done){
-      raven.utils.parseStack(stack, done);
+      raven.utils.parseStack(error, done);
     });
 
     it('should parse the correct number of frames', function(done){
-      raven.utils.parseStack(stack, function(err, frames){
-        frames.length.should.equal(10);
+      raven.utils.parseStack(error, function(err, frames) {
+        // subtract one for message line
+        var lines = error.stack.split('\n').length - 1;
+
+        // parsed frames should be the number of errors in the stack string
+        frames.length.should.equal(lines);
         done();
       });
     });
 
     it('should parse all frames correctly', function(done){
-      var expected = [{
-           'function': 'trace',
-            filename: './test/fixtures/stack.js',
-            lineno: 11,
-            pre_context: ['', 'function bar(a,b,c) {', '  var test=\'yay!\';', '  trace();', '}', '', 'function trace() {'],
-            context_line: '  console.log(__stack[1].fun.arguments);',
-            post_context: ['}', '', 'foo();', '']
-          }, {
-           'function': 'bar',
-            filename: './test/fixtures/stack.js',
-            lineno: 7,
-            pre_context: ['function foo() {', '  bar(\'hey\');', '}', '', 'function bar(a,b,c) {', '  var test=\'yay!\';'],
-            context_line: '  trace();',
-            post_context: ['}', '', 'function trace() {', '  console.log(__stack[1].fun.arguments);', '}', '', 'foo();']
-          }, {
-           'function': 'foo',
-            filename: './test/fixtures/stack.js',
-            lineno: 2,
-            pre_context: ['function foo() {'],
-            context_line: '  bar(\'hey\');',
-            post_context: ['}', '', 'function bar(a,b,c) {', '  var test=\'yay!\';', '  trace();', '}', '']
-          }, {
-           'function': 'Object.<anonymous>',
-            filename: './test/fixtures/stack.js',
-            lineno: 14,
-            pre_context: ['  trace();', '}', '', 'function trace() {', '  console.log(__stack[1].fun.arguments);', '}', ''],
-            context_line: 'foo();',
-            post_context: ['']
-          }, {
-           'function': 'Module._compile',
-            filename: 'module.js',
-            lineno: 441
-          }, {
-           'function': 'Object..js',
-            filename: 'module.js',
-            lineno: 459
-          }, {
-           'function': 'Module.load',
-            filename: 'module.js',
-            lineno: 348
-          }, {
-           'function': 'Function._load',
-            filename: 'module.js',
-            lineno: 308
-          }, {
-           'function': 'Array.0',
-            filename: 'module.js',
-            lineno: 479
-          }, {
-           'function': 'EventEmitter._tickCallback',
-            filename: 'node.js',
-            lineno: 192
-          }];
-      raven.utils.parseStack(stack, function(err, frames){
-        frames.should.eql(expected);
-        done();
-      });
-    });
-
-    it('should throw an error parsing an invalid stack', function(done){
-      raven.utils.parseStack('wtf?', function(err, frames){
-        should.exist(err);
-        err.should.be.an.instanceof(Error);
-        done();
-      });
-    });
-
-    var stacks = glob.sync(__dirname + '/fixtures/stacks/*.txt');
-    var results = glob.sync(__dirname + '/fixtures/stacks/*.json');
-    stacks.forEach(function(stackname, index) {
-      var stack = fs.readFileSync(stackname, 'utf8');
-      var result = require(results[index]);
-      it('should parse stack with '+path.basename(stackname, '.txt').replace(/_/g, ' '), function(done) {
-        raven.utils.parseStack(stack, function(err, frames){
-          frames.should.eql(result);
-          should.not.exist(err);
-          done();
+      raven.utils.parseStack(error, function(err, frames) {
+        frames[0].should.eql({
+          function: 'trace',
+          filename: './test/fixtures/stack.js',
+          lineno: 11,
+          typename: 'Object',
+          pre_context: ['', 'function bar(a,b,c) {', '  var test=\'yay!\';', '  trace();', '}', '', 'function trace() {'],
+          context_line: '  console.log(__stack[1].fun.arguments);',
+          post_context: ['}', '', 'foo();', '']
         });
+
+        frames[1].should.eql({
+          function: 'bar',
+          filename: './test/fixtures/stack.js',
+          lineno: 7,
+          typename: 'Object',
+          pre_context: ['function foo() {', '  bar(\'hey\');', '}', '', 'function bar(a,b,c) {', '  var test=\'yay!\';'],
+          context_line: '  trace();',
+          post_context: ['}', '', 'function trace() {', '  console.log(__stack[1].fun.arguments);', '}', '', 'foo();']
+        });
+
+        frames[2].should.eql({
+          function: 'foo',
+          filename: './test/fixtures/stack.js',
+          lineno: 2,
+          typename: 'Object',
+          pre_context: ['function foo() {'],
+          context_line: '  bar(\'hey\');',
+          post_context: ['}', '', 'function bar(a,b,c) {', '  var test=\'yay!\';', '  trace();', '}', '']
+        });
+
+        frames[3].should.eql({
+          function: null,
+          filename: './test/fixtures/stack.js',
+          lineno: 14,
+          typename: 'Object',
+          pre_context: ['  trace();', '}', '', 'function trace() {', '  console.log(__stack[1].fun.arguments);', '}', ''],
+          context_line: 'foo();',
+          post_context: ['']
+        });
+
+        done();
       });
     });
   });
