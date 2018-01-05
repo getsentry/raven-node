8abbf65cb95149e388ec842b79ec98285ac91f93
Neil Manvar <ndmanvar@yahoo.com>
2018-01-05 01:33:18 -0800

    Add initial test

--- test/raven.transports.js
+++ test/raven.transports.js
@@ -15,6 +15,72 @@ describe('transports', function() {
     https.agent.maxSockets.should.equal(100);
   });
 
+  it('should set proxy agent when proxy config is specified and request is sent', function(done) {
+    var option = {
+      proxyHost: 'localhost',
+      proxyPort: '8080'
+    };
+
+    // HTTP
+    var httpProxyTransport = new transports.HTTPProxyTransport(option);
+    httpProxyTransport.options.host.should.equal('localhost');
+    httpProxyTransport.options.port.should.equal('8080');
+
+    // HTTPS
+    var httpsProxyTransport = new transports.HTTPSProxyTransport(option);
+    httpsProxyTransport.options.agent.proxyOptions.should.exist;
+
+    var _cachedAgent = httpProxyTransport.agent;
+    var requests = {};
+    for (var i = 0; i < 10; i++) {
+      requests[i] = 'req';
+    }
+
+    httpProxyTransport.agent = Object.assign({}, _cachedAgent, {
+      getName: function() {
+        return 'foo:123';
+      },
+      requests: {
+        'foo:123': requests
+      }
+    });
+
+    httpsProxyTransport.send(
+      {
+        dsn: {
+          host: 'foo',
+          port: 123
+        },
+        emit: function() {}
+      },
+      null,
+      null,
+      null,
+      function() {
+        httpsProxyTransport.options.agent.proxyOptions.headers.host.should.exist;
+        done();
+      }
+    );
+
+    httpProxyTransport.send(
+      {
+        dsn: {
+          host: 'foo',
+          port: 123,
+          protocol: 'http'
+        },
+        emit: function() {}
+      },
+      null,
+      null,
+      null,
+      function() {
+        httpProxyTransport.options.path.should.contain('foo:123');
+        done();
+      }
+    );
+  });
+
   it('should emit error when requests queued over the limit', function(done) {
     var http = transports.http;
     var _cachedAgent = http.options.agent;
